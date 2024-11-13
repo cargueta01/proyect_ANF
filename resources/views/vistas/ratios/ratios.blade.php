d5bd09851f948b400974409e8aad460e227b2c6f
fern27 <c6702020@gmail.com>
2024-11-13 13:06:36 -0600

    Posición de botones de ratios

--- resources/views/vistas/ratios/ratios.blade.php
+++ resources/views/vistas/ratios/ratios.blade.php
@@ -7,8 +7,22 @@ Ratios
 
 
 @section('content')
-    <section class="section" style="margin-top: 100px">
-        
+    <section class="section">
+        <div class="section-header " style="border:0px;display:grid;grid-template-columns: repeat(2, 1fr);text-align:center;padding:5px 10px;">
+                <div style="padding: 0px 0px 10px 10px">
+                    <a href="{{ route('ratios.index') }}" class="ag-courses-item_link"style=" background-color:#212e37;border-radius: 28px;">
+                        <div class="ag-courses-item_bg"></div>
+                        <div class="ag-courses-item_title">Ratios de la empresa</div>
+                    </a>
+                </div>
+
+                <div style="padding: 0px 0px 10px 10px">
+                    <a href=" {{route('ratios.comparacion')}}" class="ag-courses-item_link"style=" border-radius: 28px;">
+                        <div class="ag-courses-item_bg"></div>
+                        <div class="ag-courses-item_title">Comparación de ratios</div>
+                    </a>
+                </div>
+        </div> 
         <div class="section-body">
             <div class="row">
                 <div class="col-lg-12">
@@ -103,25 +117,6 @@ Ratios
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="section-header " style="border:0px;display:grid;grid-template-columns: repeat(2, 1fr);text-align:center;padding:5px 10px;">
-           
-        
-      
-
-            <div style="padding: 0px 0px 10px 10px">
-                <a href="{{ route('ratios.index') }}" class="ag-courses-item_link"style=" background-color:#212e37;border-radius: 28px;">
-                    <div class="ag-courses-item_bg"></div>
-                    <div class="ag-courses-item_title">Ratios de la empresa</div>
-                </a>
-            </div>
-
-            <div style="padding: 0px 0px 10px 10px">
-                <a href=" {{route('ratios.comparacion')}}" class="ag-courses-item_link"style=" border-radius: 28px;">
-                    <div class="ag-courses-item_bg"></div>
-                    <div class="ag-courses-item_title">Comparación de ratios</div>
-                </a>
-            </div>
-        </div>
+        </div>        
     </section>
 @endsection
