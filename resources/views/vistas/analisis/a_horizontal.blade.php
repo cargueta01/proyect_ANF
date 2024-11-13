3691c36e6751787c70602b82343efa501b5cb0b0
fern27 <c6702020@gmail.com>
2024-11-13 12:31:11 -0600

    Aplicando diseño

--- resources/views/vistas/analisis/a_horizontal.blade.php
+++ resources/views/vistas/analisis/a_horizontal.blade.php
@@ -7,17 +7,19 @@ Análisis horizontal
 @section('content')
     <section class="section">
         <div class="section-header" style="display:grid;grid-template-columns: repeat(2, 1fr);text-align:center;padding:5px 10px;">
-            <div style="padding: 0px 0px 10px 10px">
-                <a class="dropdown-item has-icon" href="{{ route('horizontal.index') }}" style="padding: 5px 10px;background-color:#040525;color:#ffffff;border-radius: 10px">
-                    <h4 class="page__heading" style="margin: 0px 0px 0px 0px;">Análisis horizontal</h4>
-                </a>
+            <div style="padding: 0px 0px 10px 10px">            
+                    <a href="{{ route('horizontal.index') }}" class="ag-courses-item_link"style=" background-color:#212e37;border-radius: 28px;">
+                        <div class="ag-courses-item_bg"></div>
+                        <div class="ag-courses-item_title">Análisis horizontal</div>
+                    </a>
+                </div>
+                <div style="padding: 0px 0px 10px 10px">                
+                    <a href="{{route('vertical.index')}}" class="ag-courses-item_link"style="border-radius: 28px;">
+                        <div class="ag-courses-item_bg"></div>
+                        <div class="ag-courses-item_title">Análisis vertical</div>
+                    </a>
+                </div>
             </div>
-            <div style="padding: 0px 0px 10px 10px">
-                <a class="dropdown-item has-icon" href=" {{route('vertical.index')}}" style="padding: 5px 10px;border-radius: 10px">
-                    <h4 class="page__heading" style="margin: 0px 0px 0px 0px">Análisis vertical</h4>
-                </a>
-            </div>
-        </div>
         <div class="section-body">
             <div class="row">
                 <div class="col-lg-12">
@@ -26,7 +28,7 @@ Análisis horizontal
 
                             <div class="table-responsive">
                                 <table class="table table-striped-columns">
-                                    <thead>
+                                    <thead >
                                         <th>Cuentas</th>
                                         <th>{{$periodo1_nombre}}</th>
                                         <th>{{$periodo2_nombre}}</th>
